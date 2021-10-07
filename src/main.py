3c4718dd502b99ce0ec7bba0eb53a4b437266a5d
vicseti <seguratiradovictor@gmail.com>
2021-10-07 17:41:13 +0200

    update main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from utils import train, test, get_train_loader
 from utils import get_test_loader, visualize_stn
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--stage', type=str)
 parser.add_argument('--epochs', type=int)
 args = parser.parse_args()
 
@@ -21,10 +20,8 @@ test_loader = get_test_loader()
 model = Net().to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-if 'train' in args.stage:
-    for epoch in range(1, args.epochs + 1):
-        train(epoch, model, train_loader, device, optimizer)
+for epoch in range(1, args.epochs + 1):
+    train(epoch, model, train_loader, device, optimizer)
 
-if 'test' in args.stage:
-    test(model, test_loader, device)
-    visualize_stn(model, test_loader, device)
+test(model, test_loader, device)
+visualize_stn(model, test_loader, device)
