465535eeeec32f5a2ccc56df6d3697c31e177d09
vicseti <seguratiradovictor@gmail.com>
2021-10-07 21:03:23 +0200

    update gitignore

---
+++ src/coordconv2d.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.modules.conv as conv
+
+
+class CoordConv2d(conv.Conv2d):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True, with_r=False, use_cuda=True):
+
+        super(CoordConv2d, self).__init__(in_channels, out_channels, kernel_size,
+                                          stride, padding, dilation, groups, bias)
+        self.rank = 2
+        self.addcoords = AddCoords(self.rank, with_r, use_cuda=use_cuda)
+        self.conv = nn.Conv2d(in_channels + self.rank + int(with_r), out_channels,
+                              kernel_size, stride, padding, dilation, groups, bias)
+
+    def forward(self, input_tensor):
+
+        out = self.addcoords(input_tensor)
+        out = self.conv(out)
+        return out
