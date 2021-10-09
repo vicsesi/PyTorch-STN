949d5e6f6913ec38d6a65e79f0f99e7981c6c0ce
vicseti <seguratiradovictor@gmail.com>
2021-10-09 19:03:39 +0200

    update imgs and src

--- src/net.py
+++ src/net.py
@@ -11,9 +11,6 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        if layer == 'coordconv':
-            self.conv1 = CoordConv2d(1, 10, kernel_size=5)
-            self.conv2 = CoordConv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
